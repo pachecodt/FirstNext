1eb284f66aaba633fa5a5b06a0a0879cfbaff762
Felipe Pacheco <ext.filipe.pacheco@farfetch.com>
2021-01-22 17:56:44 +0000

    Update Routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import Link from 'next/link'
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div>
+            <div>Home</div>
+            <div><a href="/sobre">Sobre</a></div>
+        </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+function Sobre() {
+    return (
+        <div>
+            <div><a href="/">Home</a></div>
+            <div>Sobre</div>
+        </div>)
+}
+
+export default Sobre
